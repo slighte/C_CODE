482cf8f9106731cefbbc06fc0a6e99fd54efe006
王子腾 <1299374079@qq.com>
2022-12-29 00:27:38 +0800

    git

---
+++ Project1/Project1/源.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5) {
+			
+			continue;
+		}
+		printf("%d", i);
+		++i;
+	}
+	printf("\n");
+	return 0;
+
+
+
+
+
+
+}
