8ea013b86cf8346681f6830bc254194c949a8d71
王子腾 <1299374079@qq.com>
2022-12-31 21:33:55 +0800

    12.31学习笔记

---
+++ 12.31/12.31/test01.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	int ch = 0;
+//	while ((ch=getchar()) != EOF) {
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main() {
+//	int ret = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:");
+//	scanf("%s", &password);
+//	while ((ret=getchar())!='\n') {
+//		;
+//	}
+//	printf("请确认:Y/F\n");
+//	ret = getchar();
+//	if (ret == 'Y')
+//		printf("您的输入正确");
+//	else if (ret == 'F')
+//		printf("您的输入错误");
+//	else
+//		printf("请重新输入");
+//		
+//	return 0;
+//}
+//int main() {
+//	int ch = 0;
+//	while ((ch=getchar())!=EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main() {
+//	int i = 0;
+//	for (i = 1; i <= 10; i++) {
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+int main() {
+
+
+
+
+
+	return 0;
+}
+
+
