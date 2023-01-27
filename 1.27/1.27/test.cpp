67ad1bc03f76f506690611e5bd9eb28f806202f9
王子腾 <1299374079@qq.com>
2023-01-27 11:38:39 +0800

    1

---
+++ 1.27/1.27/test.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	double m = 5.0 / 2;
+//	printf("%lf\n", m);
+//	return 0;
+//}
+//int main() {
+//	int a = -1; 
+//	printf("%d\n", a >> 1);
+//	return 0;
+//}
+//int main() {
+//	int a = 3;
+//	int b = 5;
+//	printf("%d\n", a & b);
+//	printf("%d\n", a | b);	
+//	printf("%d\n", a ^ b);
+//	return 0;
+//}
+//int main() {
+//	int num = 0;
+//	int count = 0;
+
+//	while (num) {
+//		if (num % 2 == 1) {
+//			count++;
+//		}
+//		num = num / 2;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//int main() {
+//	int sum = 0;
+//	int count = 0;
+//	int i = 0;
+//	scanf("%d", &sum);
+//
+//	for (i = 0; i < 32; i++) {
+//		if (1 == ((sum >> i) & 1))
+//			count++;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//int main() {
+//	int a = 0;
+//	if (a) {
+//		printf("¹þ¹þ¹þ");
+//	}
+//	else if (!a) {
+//		printf("heheh");
+//	}
+//	return 0;
+//}
+
