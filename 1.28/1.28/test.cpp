9734fad2a5c94266d0d53798216e5946552432cb
王子腾 <1299374079@qq.com>
2023-01-30 22:53:49 +0800

    1

---
+++ 1.28/1.28/test.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	short s = 0;
+//	int a = 10;
+//	int b = 3.14;
+//	printf("%d\n", sizeof(s = a + 5));
+//	printf("%d\n", s);
+//	return 0;
+//}
+int main() {
+	char a = 3;
+	char b = 127;
+	char c = a + b;
+	printf("%d\n", c);
+	return 0;
+}
